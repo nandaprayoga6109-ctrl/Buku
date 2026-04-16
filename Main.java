223f00fca088e6a6dd0cac6a75aaf827b157167e
Gung Nanda <nandaprayoga6109@gmail.com>
2026-04-16 11:36:04 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Buku Buku1 = new Buku("Jakun terbang", "Mahesa mahendra", 2007 );
+        Buku Buku2 = new Buku ();
+
+        Buku1.tampilkan_informasi();
+       System.out.println();
+
+        Buku2.tampilkan_informasi();
+       System.out.println();
+
+    }
+}
+   
